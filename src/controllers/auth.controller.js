4b0c00615de85b61b5a84a95bbf87a87336af799
JoseLuisHernandez2004 <20221061@uthh.edu.mx>
2024-10-15 01:32:04 -0600

    login registro logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,8 @@
-  import User from '../models/user.model.js';
-  import bcrypt from 'bcrypt'
-import { log } from 'console';
-  import jwt from "jsonwebtoken";
-  import { token } from 'morgan';
+import User from '../models/user.model.js';
+import bcrypt from 'bcrypt'
+import {createAccessToken} from '../libs/jwt.js'
   
-  export const register = async (req, res) => {
+export const register = async (req, res) => {
     const {email, password, username} = req.body
       /* Crear un nuebo usuario */
     try {
@@ -18,44 +16,54 @@ import { log } from 'console';
       });
 
       const userSaved =  await newUser.save();
-
-      jwt.sign(
-        {
-          id:userSaved._id,
-        },
-        "secret123",
-
-        {
-          expiresIn: "1d",
-        },
-        (err, token)=>{
-          if(err) console.log(err);
-          res.cookie('token', token)
-          res.json({
-            message: "User created successfully",
-          })
-        }
-
-      );
-
-
-      /* res.json({
+      const token = await createAccessToken({id: userSaved._id})
+      res.cookie('token', token)
+      res.json({
         id: userSaved.id,
         username: userSaved.username,
         email: userSaved.email,
         createdAt: userSaved.createdAt,
         updatedAt: userSaved.updatedAt,
 
-      }) */
-
+      });
     } catch (error) {
-      console.log(error);
+      res.status(500).json({message: error.message});
     }
 
     //res.send('Registrando');
-  };
+};
   
-  export const login = (req, res) => {
-    res.send("login");
-  };
-  
+export const login = async (req, res) => {
+  const {email, password} = req.body
+    /* Crear un nuebo usuario */
+  try {
+
+    const userFound  = await User.findOne({ email});
+    if(!userFound) return res.status(400).json({message: "User not found"});
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) return res.status(400).json({message: "Incorrect password"});
+
+    const token = await createAccessToken({id:userFound._id});
+
+    res.cookie("token", token)
+    res.json({
+      id: userFound.id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+
+    });
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+
+  //res.send('Registrando');
+};
+export const logout = (req, res)=>{
+  res.cookie('token', "",{
+    expires: new Date(0)
+  })
+  return res.sendStatus(200);
+}
