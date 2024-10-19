5fef43c0ab8152efcdb93b26bf0659d8d9d8da26
JoseLuisHernandez2004 <20221061@uthh.edu.mx>
2024-10-19 05:26:14 -0600

    Últimos cambios en el proyecto

--- cliente/src/componentes/Navbar.js
+++ cliente/src/componentes/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = ({ isLoggedIn }) => {
         ) : (
           <Link to="/logout" className="text-red-600 hover:text-red-800">Logout</Link>
         )}
-      </div>
+      </div>    
     </nav>
   );
 };
