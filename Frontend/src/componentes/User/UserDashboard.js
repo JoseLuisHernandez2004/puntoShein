8cdfabe27a96218159daa2523f4e1011fd2da9ed
JoseLuisHernandez2004 <20221061@uthh.edu.mx>
2024-10-26 04:13:21 -0600

    Nuevos cambios

--- Frontend/src/componentes/User/UserDashboard.js
+++ Frontend/src/componentes/User/UserDashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const UserDashboard = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold">Bienvenido a tu panel de Usuario</h1>
+            <h1 className="text-4xl font-bold">Bienvenido a tu panel de Usuario!</h1>
             <p className="mt-4">Aquí puedes ver productos, revisar tus pedidos, y gestionar tu cuenta.</p>
         </div>
     );
